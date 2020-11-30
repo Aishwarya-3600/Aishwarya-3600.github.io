a5dc175106fd2d8daadd9c72dc9ca1c63da5aeb3
aishwaryard <desaiaishwaryar37@gmail.com>
2020-11-30 20:11:34 +0530

    assignment 1 subfinal commit

--- JSFiles/scripts.js
+++ JSFiles/scripts.js
@@ -1,24 +1,11 @@
-/*function f1(){
-var About = document.getElementById("about");
-var More = document.getElementById("more");
-var Contact = document.getElementById("contact");
-if (About.style.display === "none"){
-    About.style.display = "block";
-}
-else{
-    More.style.display =
-    Contact.style.display = 
-}
-  }*/
-  /*$(".js-scroll-trigger").click(function () {
-    $(".navbar-collapse").collapse("hide");
-});*/
-
-  
-  
-  $("#about").click(function(){
-    $("#about").show();
-    $("#more").hide();
-    $("#contact").hide();
-  });
+$(document).ready(function(){
+var tB = document.getElementsByClassName('toggle-button')[0]
+var nL = document.getElementsByClassName('navbar-links')[0]
+console.log('hello')
+console.log('2')
+tB.addEventListener('click',() => {
+  nL.classList.toggle('active')
+  console.log('1')
+})
+})
 
