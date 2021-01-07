554131b6bf840f65dd22d27a9c7f4bda89efb1c1
aishwaryard <desaiaishwaryar37@gmail.com>
2021-01-07 23:39:05 +0530

    toto

--- my-app/src/ToDoList.js
+++ my-app/src/ToDoList.js
@@ -1,15 +1,35 @@
-import react from 'react'
+import { useState } from 'react';
+
 const ToDoList = (props) => {
+    const [done, setDone] = useState(false);
+    const [itemText, setText] = useState(props.text);
+    const markAsDone = () => {
+        setDone(() => {
+            return true;
+        });
+    }
+    const makeEdit = () => {
+        var editItem = prompt("Enter editted item: ");
+        setText(() => {
+            return editItem;
+        })
+    }
     return (
         <div class="todo_style">
-            <i class="fa fa-times" aria-hidden="true" onClick = { () => {
-                                            props.onSelect(props.id) }}/>
-    <li> {props.text} </li>
-    <br/><br/><br/>
-    </div>);
+            <i class={done ? "fa fa-check" : "fa fa-times"} id="cross" aria-hidden="true" onClick={() => {
+                props.onSelect(props.id)
+            }} />
+            <li>
+                <p id="item">{itemText} </p>
+                <p>
+                    <input type="time" class="form-control timer" id="start" />
+                </p></li>
+            <button class="edit" onClick={makeEdit}>
+                Edit</button>
+            <br /><br /><br />
+            <button class="done" onClick={markAsDone}>Done</button>
+        </div>);
 
-    };
-
-
+};
 
 export default ToDoList;
